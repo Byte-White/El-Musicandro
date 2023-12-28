a0fde829c8b0254dab12559ea04930961d874751
Byte-Wh1te <bytewh1te@yahoo.com>
2023-12-29 01:54:32 +0200

    added favourites list + new command

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,30 @@ require('dotenv').config();
 const { Client, IntentsBitField } = require("discord.js")
 const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource, StreamType } = require('@discordjs/voice');
 const ytdl = require('ytdl-core');
+const fs = require('fs');
 
+
+function getRandomLine(filePath) {
+  const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+  
+  // Check if the file has at least one line
+  if (lines.length === 0) {
+    return 'File is empty';
+  }
+
+  const randomIndex = Math.floor(Math.random() * lines.length);
+  return lines[randomIndex];
+}
+function getLine(filePath,index)
+{
+    const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+    if (index >= 0 && index < lines.length) {
+        return lines[index];
+    }
+    else{
+        return getRandomLine(filepath);
+    }
+}
 
 
 const client = new Client({
@@ -55,7 +78,17 @@ client.on('messageCreate',(message)=>{
                     const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
                     console.log(resource);
                     audioPlayer.play(resource);
+                    // Listen for the audio player to finish playing
+                    audioPlayer.on('stateChange', (oldState, newState) => {
+                    if (newState.status === 'idle') {
+                            // Cleanup after the song has finished playing
+                            audioPlayer.stop();
+                            connection.destroy();
+                            console.log(`🎵 Finished playing ${args[0]}`);
+                        }
+                    });
                     connection.subscribe(audioPlayer);
+                    message.channel.send("reproduciendo ahora " + args[0]);
                 }
                 else{
                     console.log(`🔵Direct MP3 File from ${args[0]}`);
@@ -95,7 +128,48 @@ client.on('messageCreate',(message)=>{
         });
         console.log(`🚪Joined ${voiceChannel.name}`);
     }
+    if (message.content.startsWith('!freestyle'))
+    {
+        const [command, ...args] = message.content.split(' ');
+        const voiceChannel = message.member.voice.channel;
+        const member = message.guild.members.cache.get(message.author.id)
+        if (!voiceChannel) {
+            return message.channel.send("Amigo, únete a un canal de voz primero.");
+        }
+        const connection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: voiceChannel.guild.id,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        });
+        let song;
+        if (args.length === 0) {
+        song = getRandomLine("favourites.txt");
+        }
+        else{
+            song = getLine("favourites.txt",parseInt(args[0]));
+        }
+        console.log(`🔴USING YTDL for ${song}`);
+                    const stream = ytdl(song, { filter: 'audioonly' }); // Use ytdl-core to get the audio stream
+                    stream.on('error', (error) => {
+                        console.error(`ytdl-core error: ${error.message}`);
+                    });
+        const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
+        console.log(resource);
+        audioPlayer.play(resource);
 
+        // Listen for the audio player to finish playing
+        audioPlayer.on('stateChange', (oldState, newState) => {
+            if (newState.status === 'idle') {
+                // Cleanup after the song has finished playing
+                audioPlayer.stop();
+                connection.destroy();
+                console.log(`🎵 Finished playing ${song}`);
+            }
+        });
+
+        connection.subscribe(audioPlayer);
+        message.channel.send("reproduciendo ahora " + song);
+    }
 
 }
 )
